ccce8840a3790a3b61ddcca37456b80a0dc321b0
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-01 15:01:29 +0100

    Prepped for adding game tests

---
+++ project/app/views/game/spec/gameSpec.js
@@ -0,0 +1,18 @@
+
+describe("Game", function() {
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+
+
+  describe("Can create a frame", function() {
+    // it("has two rolls as limit", function() {
+    //   expect(frame.getRollLimit()).toEqual(2);
+    // });
+
+
+  });
+});
