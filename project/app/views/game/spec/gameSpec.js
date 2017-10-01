f429a1dd7951acf8b54c91a4542b5ea2c3e5edef
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-01 17:50:40 +0100

    11th frame is only implemented, and only with number of rolls, if in a bonus round

--- project/app/views/game/spec/gameSpec.js
+++ project/app/views/game/spec/gameSpec.js
@@ -8,13 +8,13 @@ describe("Game", function() {
   describe("Can interact with frame", function() {
     it("can create new Frames", function() {
       newFrame = game.newFrame(2);
-      expect(newFrame.getRollLimit()).toEqual(2);
+      expect(game.frameArray).toEqual(newFrame);
     });
 
     it("creates frames with roll scorers based on limit", function() {
-      newFrame = game.newFrame(2);
-      newFrame.createRollScores(2);
-      expect(newFrame.getRollScores()).toEqual([0, 0]);
+      game.newFrame(2);
+      aNewFrame = game.frameArray.slice(-1)[0];
+      expect(aNewFrame.getRollScores()).toEqual([0, 0]);
     });
 
     it("can count 0 frames at start", function() {
@@ -31,11 +31,31 @@ describe("Game", function() {
       expect(game.getFrameCounter()).toEqual(10);
     });
 
-    it("stops creating frames at 10", function() {
+    it("stops creating frames at 11", function() {
       for (i = 0; i < 11; i++) {
         newFrame = game.newFrame(2);
       }
       expect(game.newFrameRules(2)).toEqual("Game over!");
+      expect(game.frameArray.length).toEqual(11);
+    });
+
+    it("stops creating frames at 10", function() {
+      for (i = 0; i < 10; i++) {
+        newFrame = game.newFrame(2);
+      }
+      expect(game.frameArray.length).toEqual(10);
+      expect(game.frameArray.slice(-1)[0].getRollScores()).toEqual([0,0]);
+    });
+
+    it("stops creating frames at 10 unless bonus status is true", function() {
+      for (i = 0; i < 10; i++) {
+        newFrame = game.newFrame(2);
+      }
+      game.STRIKE = true;
+      expect(game.newFrameRules(2)).toEqual(game.frameArray);
+      // test above just checks that expected return value of an updated array is created
+      expect(game.frameArray.length).toEqual(11);
+      expect(game.frameArray.slice(-1)[0].getRollScores()).toEqual([0,0]);
     });
   });
 });
--- project/app/views/game/src/bowling_game.js
+++ project/app/views/game/src/bowling_game.js
@@ -2,18 +2,28 @@
 
 function Game() {
   this.frameCounter = 0;
+  this.frameArray = [];
+  this.SPARE_NUMBER = 1;
+  this.STRIKE_NUMBER = 2;
+  this.SPARE = false;
+  this.STRIKE = false;
 }
 
 Game.prototype.newFrame = function(number) {
+  var frame = new Frame(number);
+  this.frameArray.push(frame);
+
+  frame.createRollScores(number);
   this.frameCounter += 1;
-  return new Frame(number);
+
+  return this.frameArray;
 };
 
 Game.prototype.newFrameRules = function(number) {
   if (this.frameCounter > 10) {
     return "Game over!";
-  } else if (this.frameCounter === 10) {
-    return this.newFrame(number);
+  } else if (this.frameCounter === 10 && this.bonusStatus() === true) {
+    return this.newFrame(this.bonusValue());
   } else {
     return this.newFrame(number);
   }
@@ -21,4 +31,16 @@ Game.prototype.newFrameRules = function(number) {
 
 Game.prototype.getFrameCounter = function() {
   return this.frameCounter;
+};
+
+Game.prototype.bonusStatus = function() {
+  return this.SPARE || this.STRIKE;
+};
+
+Game.prototype.bonusValue = function() {
+  if (this.SPARE === true) {
+    return this.SPARE_NUMBER;
+  } else {
+    return this.STRIKE_NUMBER;
+  }
 };
