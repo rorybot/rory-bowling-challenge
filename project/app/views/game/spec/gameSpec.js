d700fc4b0828ff39697340f767efc3d27ec28323
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-01 15:42:05 +0100

    Passing test to not create past 10 frames

--- project/app/views/game/spec/gameSpec.js
+++ project/app/views/game/spec/gameSpec.js
@@ -1,4 +1,3 @@
-
 describe("Game", function() {
   var game;
 
@@ -6,33 +5,38 @@ describe("Game", function() {
     game = new Game();
   });
 
-
-
   describe("Can interact with frame", function() {
     it("can create new Frames", function() {
-      newFrame = game.newFrame(2)
+      newFrame = game.newFrame(2);
       expect(newFrame.getRollLimit()).toEqual(2);
     });
 
     it("creates frames with roll scorers based on limit", function() {
-      newFrame = game.newFrame(2)
-      newFrame.createRollScores(2)
-      expect(newFrame.getRollScores()).toEqual([0,0]) ;
+      newFrame = game.newFrame(2);
+      newFrame.createRollScores(2);
+      expect(newFrame.getRollScores()).toEqual([0, 0]);
     });
 
     it("can count 0 frames at start", function() {
       // for (i = 0; i < 10; i++) {
       // newFrame = game.newFrame(2) ;
       // }
-      expect(game.getFrameCounter()).toEqual(0) ;
+      expect(game.getFrameCounter()).toEqual(0);
     });
 
     it("can count 10 frames", function() {
       for (i = 0; i < 10; i++) {
-      newFrame = game.newFrame(2) ;
+        newFrame = game.newFrame(2);
       }
-      expect(game.getFrameCounter()).toEqual(10) ;
+      expect(game.getFrameCounter()).toEqual(10);
     });
 
+    it("stops creating frames at 10", function() {
+      for (i = 0; i < 10; i++) {
+        newFrame = game.newFrame(2);
+      }
+
+      expect(game.newFrame(2)).toEqual("Go home - you've reached your limit!");
+    });
   });
 });
--- project/app/views/game/src/bowling_game.js
+++ project/app/views/game/src/bowling_game.js
@@ -1,15 +1,18 @@
-'use strict';
+"use strict";
 
 function Game() {
-  this.frameCounter = 0
+  this.frameCounter = 0;
 }
 
-
-Game.prototype.newFrame = function(number){
-  this.frameCounter += 1;
-  return new Frame(2);
+Game.prototype.newFrame = function(number) {
+  if (this.frameCounter === 10) {
+    return "Go home - you've reached your limit!";
+  } else {
+    this.frameCounter += 1;
+    return new Frame(2);
+  }
 };
 
-Game.prototype.getFrameCounter = function () {
- return this.frameCounter;
+Game.prototype.getFrameCounter = function() {
+  return this.frameCounter;
 };
